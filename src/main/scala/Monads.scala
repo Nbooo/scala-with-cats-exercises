bd5893482adf51aed27a7b6ab1649b432ec9a181
nbooo <nbovenko@gmail.com>
2019-01-07 14:33:02 +0200

    Chapter 4. Working through Monads #3

--- src/main/scala/Monads.scala
+++ src/main/scala/Monads.scala
@@ -5,10 +5,10 @@ import cats.syntax.flatMap._
 import cats.syntax.applicative._
 import cats.syntax.writer._
 import cats.instances.vector._
+
 import scala.language.higherKinds
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 trait MyMonad[F[_]] {
@@ -77,5 +77,45 @@ object Monads {
   }
 }
 
+object CustomMonads {
+  import Tree._
 
+  implicit val treeMonad: Monad[Tree] = new Monad[Tree] {
+    override def pure[A](x: A): Tree[A] = Leaf(x)
+
+    // 2^n complexity, n - depth of a tree.
+    override def flatMap[A, B](fa: Tree[A])(f: A => Tree[B]): Tree[B] = {
+      fa match {
+        case Branch(left, right)  => Branch(flatMap(left)(f), flatMap(right)(f))
+        case Leaf(a)              => f(a)
+      }
+    }
+
+    override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
+      ???
+    }
+
+    private def tailRec[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
+      def unbox(tree: Tree[Either[A, B]]): Option[Tree[B]] = tree match {
+        case Branch(left, right)  => ???
+        case Leaf(value)          => value match {
+          case Left(_)  => None
+          case Right(b) => Some(Leaf(b))
+        }
+      }
+
+      def mergeTuple(l: Option[Tree[B]], r: Option[Tree[B]]): Option[Tree[B]] = (l, r) match {
+        case (Some(left), Some(right))  => Some(Branch(left, right))
+        case _                          => None
+      }
+
+      f(a) match {
+        case Branch(left, right) => mergeTuple(unbox(left), unbox(right)) match {
+          case Some(tree) => tree
+          case _          => tailRec(a)(f)
+        }
+      }
+    }
+  }
+}
 
